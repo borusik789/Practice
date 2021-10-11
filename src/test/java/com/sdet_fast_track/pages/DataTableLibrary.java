096f2f02d737a3ca39d90fd00eb8df81b4566ea9
Erjon Domi <erjondomi@hotmail.com>
2021-10-11 19:04:11 -0400

    test created

--- src/test/java/com/sdet_fast_track/pages/DataTableLibrary.java
+++ src/test/java/com/sdet_fast_track/pages/DataTableLibrary.java
@@ -11,8 +11,29 @@ public class DataTableLibrary {
      *  When User enters below info
      *  |username|Tester|
      *  |password|test |
-     *  Then User should see title changed to Web
-     * Orders
+     *  Then User should see title changed to Web Orders
+     */
+
+    /**
+     * TC##0020  http://practice.cybertekschool.com/dropdown
+     * Feature: Simple examples of how dataTables work
+     *
+     *   @dataTable
+     *   Scenario: User should be able to see all 12 months in months dropdown
+     *     Given User is on the dropdowns page of practice tool
+     *     Then User should see below info in month dropdown
+     *       | January   |
+     *       | February  |
+     *       | March     |
+     *       | April     |
+     *       | May       |
+     *       | June      |
+     *       | July      |
+     *       | August    |
+     *       | September |
+     *       | October   |
+     *       | November  |
+     *       | December  |
      */
 
     /**
