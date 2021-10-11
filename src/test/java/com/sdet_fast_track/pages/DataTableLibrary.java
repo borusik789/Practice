73eb88ef658e7f87d6d4b29f452f682881f750c2
Erjon Domi <erjondomi@hotmail.com>
2021-10-11 18:53:23 -0400

    Project created

---
+++ src/test/java/com/sdet_fast_track/pages/DataTableLibrary.java
@@ -0,0 +1,41 @@
+package com.sdet_fast_track.pages;
+
+public class DataTableLibrary {
+
+    /**
+     * http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx
+     * C#27: SmartBear order process
+     * Scenario: User should be able to login with
+     * correct credentials
+     *  Given User is on the login page of SmartBear
+     *  When User enters below info
+     *  |username|Tester|
+     *  |password|test |
+     *  Then User should see title changed to Web
+     * Orders
+     */
+
+    /**
+     * TC#4: Data tables task
+     * Task summary:
+     * -> Create a new feature file named ShowRecords.feature
+     * -> Create a new scenario.
+     * -> Login as a librarian
+     * -> Click on Users link
+     * -> Show records dropdown value should be 10 by default.
+     * -> And show records should have following options: 5, 10, 15, 50, 100, 200, 500
+     *
+     *
+     * TC#5: Data tables task
+     * Task summary:
+     * -> Create a new feature file named SearchResults.feature
+     * -> As a user, I should be able to login as librarian. When I go to Users page, table
+     * should have following columns:
+     * • Actions
+     * • UserID
+     * • Full Name
+     * • Email
+     * • Group
+     * • Status
+     */
+}
