6759b7bb9e565e9101dab9ffa60cd2c616324fb2
Erjon Domi <erjondomi@hotmail.com>
2021-10-12 20:17:45 -0400

    username and password added

--- src/test/java/com/sdet_fast_track/pages/EtsyPage.java
+++ src/test/java/com/sdet_fast_track/pages/EtsyPage.java
@@ -9,12 +9,15 @@ public class EtsyPage {
      * Expected: Etsy - Shop for handmade, vintage, custom, and unique gifts for
      * everyone
      * Note: Follow POM
+     *
      * TC#52: Etsy Search Functionality Title Verification (without parameterization)
      * 1. User is on https://www.etsy.com
      * 2. User types Wooden Spoon in the search box
      * 3. User clicks search button
      * 4. User sees Wooden Spoon is in the title
      * Note: Follow POM
+     *
+     *
      * TC#53: Etsy Search Functionality Title Verification (with parameterization)
      * 1. User is on https://www.etsy.com
      * 2. User types Wooden Spoon in the search box
